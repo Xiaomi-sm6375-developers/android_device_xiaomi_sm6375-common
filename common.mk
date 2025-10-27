55affc1183627f7e33b139dbc0ab6f211d623b6a
Onelots <onelots@onelots.fr>
2025-10-27 21:13:40 +0100

    sm6375-common: Drop Dolby
    This reverts commit 7676ec5b1777f580c67cad7d1bf0c430f592c158.

--- common.mk
+++ common.mk
@@ -81,7 +81,6 @@ PRODUCT_COPY_FILES += \
      $(call find-copy-subdir-files,*,$(LOCAL_PATH)/audio,$(TARGET_COPY_OUT_VENDOR)/etc/audio/$(AUDIO_SKU_DIR))
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/audio/dax-default.xml:$(TARGET_COPY_OUT_VENDOR)/etc/dolby/dax-default.xml \
     $(AUDIO_HAL_DIR)/configs/holi/audio_tuning_mixer.txt:$(TARGET_COPY_OUT_VENDOR)/etc/audio_tuning_mixer.txt
 
 PRODUCT_COPY_FILES += \
@@ -130,10 +129,6 @@ PRODUCT_PACKAGES += \
 
 # DebugFS
 PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
-
-# Device-specific settings
-PRODUCT_PACKAGES += \
-    XiaomiDolby
 
 # Device as Webcam
 PRODUCT_PACKAGES += \
--- extract-files.py
+++ extract-files.py
@@ -60,11 +60,6 @@ blob_fixups: blob_fixups_user_type = {
     
     ('vendor/lib64/libwvhidl.so', 'vendor/lib64/mediadrm/libwvdrmengine.so'): blob_fixup()
         .add_needed('libcrypto_shim.so'),
-    
-    ('vendor/lib64/libdlbdsservice.so', 
-     'vendor/lib/libstagefright_soft_ac4dec.so', 
-     'vendor/lib/libstagefright_soft_ddpdec.so'): blob_fixup()
-        .replace_needed('libstagefright_foundation.so', 'libstagefright_foundation-v33.so'),
 
     'vendor/etc/libnfc-sn100.conf': blob_fixup()
         .regex_replace(r'(DEFAULT_ISODEP_ROUTE.*?)0x01', r'\g<1>0xC0')
